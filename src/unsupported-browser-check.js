915801d8dd56018bc77b6f0365d02da179fbc14b
Yona Appletree <yona@concentricsky.com>
2018-12-19 11:30:35 -0800

    BGR-2828: Improved unsupported browser warning script to not require an inline script reference

---
+++ src/unsupported-browser-check.js
@@ -0,0 +1,9 @@
+// Simple feature-based check for old browsers (like IE11)
+if (typeof Symbol === "undefined") {
+	const request = new XMLHttpRequest();
+	request.onload = function() {
+		document.write(request.responseText);
+	};
+	request.open("GET", "/unsupported-browser-warning.html");
+	request.send();
+}
