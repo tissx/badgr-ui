e6da8a5184ee5d9409b90209122fdb44eba073bb
Nathan Nichols <nnichols@concentricsky.com>
2019-01-23 12:53:03 -0800

    wrap up issuer detail

--- src/app/issuer/components/issuer-detail/issuer-detail.component.ts
+++ src/app/issuer/components/issuer-detail/issuer-detail.component.ts
@@ -1,38 +1,38 @@
-import {Component, OnInit} from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
-import {ActivatedRoute, Router} from "@angular/router";
-import {SessionService} from "../../../common/services/session.service";
-import {BaseAuthenticatedRoutableComponent} from "../../../common/pages/base-authenticated-routable.component";
-import {MessageService} from "../../../common/services/message.service";
-import {IssuerManager} from "../../services/issuer-manager.service";
-import {BadgeClassManager} from "../../services/badgeclass-manager.service";
-import {Issuer} from "../../models/issuer.model";
-import {BadgeClass} from "../../models/badgeclass.model";
-import {Title} from "@angular/platform-browser";
-import {LearningPathway} from "../../models/pathway.model";
-import {PathwayManager} from "../../services/pathway-manager.service";
-import {RecipientGroup} from "../../models/recipientgroup.model";
-import {RecipientGroupManager} from "../../services/recipientgroup-manager.service";
-import {preloadImageURL} from "../../../common/util/file-util";
-import {UserProfileManager} from "../../../common/services/user-profile-manager.service";
-import {UserProfileEmail} from "../../../common/model/user-profile.model";
+import { ActivatedRoute, Router } from '@angular/router';
+import { SessionService } from '../../../common/services/session.service';
+import { BaseAuthenticatedRoutableComponent } from '../../../common/pages/base-authenticated-routable.component';
+import { MessageService } from '../../../common/services/message.service';
+import { IssuerManager } from '../../services/issuer-manager.service';
+import { BadgeClassManager } from '../../services/badgeclass-manager.service';
+import { Issuer } from '../../models/issuer.model';
+import { BadgeClass } from '../../models/badgeclass.model';
+import { Title } from '@angular/platform-browser';
+import { LearningPathway } from '../../models/pathway.model';
+import { PathwayManager } from '../../services/pathway-manager.service';
+import { RecipientGroup } from '../../models/recipientgroup.model';
+import { RecipientGroupManager } from '../../services/recipientgroup-manager.service';
+import { preloadImageURL } from '../../../common/util/file-util';
+import { UserProfileManager } from '../../../common/services/user-profile-manager.service';
+import { UserProfileEmail } from '../../../common/model/user-profile.model';
 
-import {ApiExternalToolLaunchpoint} from "app/externaltools/models/externaltools-api.model";
-import {ExternalToolsManager} from "app/externaltools/services/externaltools-manager.service";
-import {AppConfigService} from "../../../common/app-config.service";
-
+import { ApiExternalToolLaunchpoint } from 'app/externaltools/models/externaltools-api.model';
+import { ExternalToolsManager } from 'app/externaltools/services/externaltools-manager.service';
+import { AppConfigService } from '../../../common/app-config.service';
 
 @Component({
 	selector: 'issuer-detail',
-	templateUrl: './issuer-detail.component.html',
+	templateUrl: './issuer-detail.component.html'
 })
 export class IssuerDetailComponent extends BaseAuthenticatedRoutableComponent implements OnInit {
-	readonly issuerImagePlaceHolderUrl = preloadImageURL(require('../../../../breakdown/static/images/placeholderavatar-issuer.svg'));
+	readonly issuerImagePlaceHolderUrl = preloadImageURL(
+		require('../../../../breakdown/static/images/placeholderavatar-issuer.svg')
+	);
 
 	issuer: Issuer;
 	issuerSlug: string;
 	badges: Array<BadgeClass>;
-	confirmingBadgeId: number;
 	confirmingRecipientGroup: RecipientGroup;
 	launchpoints: ApiExternalToolLaunchpoint[];
 
@@ -63,53 +63,58 @@ export class IssuerDetailComponent extends BaseAuthenticatedRoutableComponent im
 	) {
 		super(router, route, loginService);
 
-		title.setTitle(`Issuer Detail - ${this.configService.theme['serviceName'] || "Badgr"}`);
+		title.setTitle(`Issuer Detail - ${this.configService.theme['serviceName'] || 'Badgr'}`);
 
 		this.issuerSlug = this.route.snapshot.params['issuerSlug'];
 
-		this.externalToolsManager.getToolLaunchpoints("issuer_external_launch").then(launchpoints => {
-			this.launchpoints = launchpoints.filter(lp => Boolean(lp));
+		this.externalToolsManager.getToolLaunchpoints('issuer_external_launch').then((launchpoints) => {
+			this.launchpoints = launchpoints.filter((lp) => Boolean(lp));
 		});
 
 		this.issuerLoaded = this.issuerManager.issuerBySlug(this.issuerSlug).then(
 			(issuer) => {
 				this.issuer = issuer;
-				this.title.setTitle(`Issuer - ${this.issuer.name} - ${this.configService.theme['serviceName'] || "Badgr"}`);
+				this.title.setTitle(
+					`Issuer - ${this.issuer.name} - ${this.configService.theme['serviceName'] || 'Badgr'}`
+				);
 
 				this.badgesLoaded = new Promise((resolve, reject) => {
 					this.badgeClassService.badgesByIssuerUrl$.subscribe(
-						badgesByIssuer => {
-							const cmp = (a,b) => a == b ? 0 : (a < b ? -1 : 1);
-							this.badges = (badgesByIssuer[ this.issuer.issuerUrl ] || []).sort((a,b) => cmp(b.createdAt, a.createdAt))
+						(badgesByIssuer) => {
+							const cmp = (a, b) => (a == b ? 0 : a < b ? -1 : 1);
+							this.badges = (badgesByIssuer[this.issuer.issuerUrl] || [])
+								.sort((a, b) => cmp(b.createdAt, a.createdAt));
 							resolve();
 						},
-						error => {
+						(error) => {
 							this.messageService.reportAndThrowError(
-								`Failed to load badges for ${this.issuer ? this.issuer.name : this.issuerSlug}`, error
+								`Failed to load badges for ${this.issuer ? this.issuer.name : this.issuerSlug}`,
+								error
 							);
 							resolve();
 						}
 					);
 				});
-			}, error => {
+			},
+			(error) => {
 				this.messageService.reportLoadingError(`Issuer '${this.issuerSlug}' does not exist.`, error);
 			}
 		);
 
-		this.groupsLoaded = this.recipientGroupManager.loadRecipientGroupsForIssuer(this.issuerSlug)
-			.then(
-				groups => {
-					this.recipientGroups = groups.entities
-				},
-				error => this.messageService.reportAndThrowError(`Failed to fetch Recipient Groups`, error)
-			);
+		this.groupsLoaded = this.recipientGroupManager.loadRecipientGroupsForIssuer(this.issuerSlug).then(
+			(groups) => {
+				this.recipientGroups = groups.entities;
+			},
+			(error) => this.messageService.reportAndThrowError(`Failed to fetch Recipient Groups`, error)
+		);
 
-		this.pathwaysLoaded = this.pathwayManager.loadPathwaysForIssuer(this.issuerSlug)
-			.then(pathways => this.pathways = pathways.entities);
+		this.pathwaysLoaded = this.pathwayManager
+			.loadPathwaysForIssuer(this.issuerSlug)
+			.then((pathways) => (this.pathways = pathways.entities));
 
 		this.profileEmailsLoaded = this.profileManager.userProfilePromise
-			.then(profile => profile.emails.loadedPromise)
-			.then(emails => this.profileEmails = emails.entities);
+			.then((profile) => profile.emails.loadedPromise)
+			.then((emails) => (this.profileEmails = emails.entities));
 	}
 
 	ngOnInit() {
@@ -117,53 +122,32 @@ export class IssuerDetailComponent extends BaseAuthenticatedRoutableComponent im
 	}
 
 	get legacyPathwaysVisible(): boolean {
-		return this.pathwaysLoaded && this.pathways.length > 0
+		return this.pathwaysLoaded && this.pathways.length > 0;
 	}
 
 	deleteRecipientGroup(group: RecipientGroup) {
-		group.deleteRecipientGroup().then(
-			() => this.messageService.reportMinorSuccess(`Deleted recipient group ${group.name}`),
-			error => this.messageService.reportAndThrowError(`Failed to delete recipient group ${group.name}`)
-		)
+		group
+			.deleteRecipientGroup()
+			.then(
+				() => this.messageService.reportMinorSuccess(`Deleted recipient group ${group.name}`),
+				(error) => this.messageService.reportAndThrowError(`Failed to delete recipient group ${group.name}`)
+			);
 	}
 
-	clickCancelRemove(ev) {
-		this.confirmingBadgeId = null;
-	}
-
-	clickConfirmRemove(ev, badge) {
-		if (badge.recipientCount > 0) {
-			ev.preventDefault();
-		} else {
-			this.confirmingBadgeId = badge.id;
-		}
-	}
-
-	clickRemove(ev, badge) {
-		this.badgeClassService.removeBadgeClass(badge).then(() => {
-			this.messageService.setMessage("Removed badge " + badge.name, "success");
-		}, error => {
-			this.messageService.setMessage("Unable to remove " + badge.name, "error");
-		})
-	}
-
-	deletePathway(ev, pathway: LearningPathway) {
-		pathway.deletePathway();
-	}
-
-	updateGroupActiveState(
-		recipientGroup: RecipientGroup,
-		active: boolean
-	) {
+	updateGroupActiveState(recipientGroup: RecipientGroup, active: boolean) {
 		recipientGroup.active = active;
-		recipientGroup.save().then(
-			() => this.messageService.reportMinorSuccess(
-				`${active ? 'Activated' : 'Deactivated'} recipient group ${recipientGroup.name}`
-			),
-			error => this.messageService.reportAndThrowError(
-				`Failed to ${active ? 'activate' : 'deactivate'} recipient group ${recipientGroup.name}`,
-				error
-			)
-		)
+		recipientGroup
+			.save()
+			.then(
+				() =>
+					this.messageService.reportMinorSuccess(
+						`${active ? 'Activated' : 'Deactivated'} recipient group ${recipientGroup.name}`
+					),
+				(error) =>
+					this.messageService.reportAndThrowError(
+						`Failed to ${active ? 'activate' : 'deactivate'} recipient group ${recipientGroup.name}`,
+						error
+					)
+			);
 	}
 }
