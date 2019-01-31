596369a8696d29bd1def9af5270161e79328d1a3
Nathan Nichols <nnichols@concentricsky.com>
2019-01-31 10:53:58 -0800

    adjusts formatting, moves shared inline styles to constants

--- src/embed/generate-embed-html.ts
+++ src/embed/generate-embed-html.ts
@@ -1,108 +1,144 @@
-
 const checkmarkSvg = require("../breakdown/static/images/checkmark-circle.svg");
 
 export function generateEmbedHtml(embedOptions) {
-    const options = embedOptions || {
-        shareUrl: null,
-        imageUrl: null,
-        includeBadgeClassName: false,
-        badgeClassName: null,
-        includeAwardDate: false,
-        awardDate: null,
-        includeRecipientName: false,
-        recipientName: null,
-        recipientIdentifier: null,
-        recipientType: null,
-        includeVerifyButton: false,
-        verified: false,
-        includeScript: true,
-        staticPrefix: null,
-    };
+	const options = embedOptions || {
+		shareUrl: null,
+		imageUrl: null,
+		includeBadgeClassName: false,
+		badgeClassName: null,
+		includeAwardDate: false,
+		awardDate: null,
+		includeRecipientName: false,
+		recipientName: null,
+		recipientIdentifier: null,
+		recipientType: null,
+		includeVerifyButton: false,
+		verified: false,
+		includeScript: true,
+		staticPrefix: null
+	};
 
-    const shareUrl = embedOptions.recipientIdentifier ? (options.shareUrl+"&identity__"+(embedOptions.recipientType || "email")+"="+embedOptions.recipientIdentifier) : options.shareUrl;
+	const shareUrl = embedOptions.recipientIdentifier
+		? options.shareUrl +
+		  "&identity__" +
+		  (embedOptions.recipientType || "email") +
+		  "=" +
+		  embedOptions.recipientIdentifier
+		: options.shareUrl;
 
-    const blockquote = document.createElement("blockquote");
-    blockquote.className = "badgr-badge";
-    blockquote.setAttribute("style", 'font-family: Helvetica, Roboto, \"Segoe UI\", Calibri, sans-serif;');
+	const blockquote = document.createElement("blockquote");
+	blockquote.className = "badgr-badge";
+	blockquote.setAttribute(
+		"style",
+		'font-family: Helvetica, Roboto, "Segoe UI", Calibri, sans-serif;'
+	);
 
-    const a = document.createElement("a");
-    a.href = shareUrl;
-    const img = document.createElement("img");
-    img.setAttribute("width", "120px");
-    img.setAttribute("height", "120px");
-    img.src = options.imageUrl;
-    a.appendChild(img);
+	const a = document.createElement("a");
+	a.href = shareUrl;
+	const img = document.createElement("img");
+	img.setAttribute("width", "120px");
+	img.setAttribute("height", "120px");
+	img.src = options.imageUrl;
+	a.appendChild(img);
     blockquote.appendChild(a);
+    
+    // Inline Styles
+    const badgeTitleStyle = "margin: 0; font-size: 16px; font-weight: 600; font-style: normal; font-stretch: normal; line-height: 1.25; letter-spacing: normal; text-align: left; color: #05012c;";
+    const optionNameStyle = "font-size: 12px; font-weight: bold; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #000;";
+    const optionValueStyle = "margin: 0; font-size: 12px; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #555555;";
+    const verifyButtonStyle = "margin: 0; font-size:14px; font-weight: bold; width: 48px; height: 16px; border-radius: 4px; border: solid 1px black; text-decoration: none; padding: 6px 16px; margin: 16px 0; color: black;";
 
-    if (options.includeBadgeClassName && options.badgeClassName) {
-        const nameP = document.createElement("p");
-        nameP.className = "badgr-badge-name";
-        nameP.setAttribute("style", "margin: 0; font-size: 16px; font-weight: 600; font-style: normal; font-stretch: normal; line-height: 1.25; letter-spacing: normal; text-align: left; color: #05012c;");
-        nameP.innerHTML = options.badgeClassName;
-        blockquote.appendChild(nameP);
-    }
+	if (options.includeBadgeClassName && options.badgeClassName) {
+		const nameP = document.createElement("p");
+		nameP.className = "badgr-badge-name";
+		nameP.setAttribute(
+			"style",
+			badgeTitleStyle
+		);
+		nameP.innerHTML = options.badgeClassName;
+		blockquote.appendChild(nameP);
+	}
 
-    if (options.includeAwardDate) {
-        const dateP = document.createElement("p");
-        dateP.className = "badgr-badge-date";
-        const dateStrong = document.createElement("strong");
-        dateStrong.setAttribute("style", "font-size: 12px; font-weight: bold; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #000;");
-        dateStrong.innerHTML = "Awarded:";
-        dateP.appendChild(dateStrong);
-        dateP.setAttribute("style", "margin: 0; font-size: 12px; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #555555;");
+	if (options.includeAwardDate) {
+		const dateP = document.createElement("p");
+		dateP.className = "badgr-badge-date";
+		const dateStrong = document.createElement("strong");
+		dateStrong.setAttribute(
+			"style",
+			optionNameStyle
+		);
+		dateStrong.innerHTML = "Awarded:";
+		dateP.appendChild(dateStrong);
+		dateP.setAttribute(
+			"style",
+			optionValueStyle
+		);
 
-        dateP.innerHTML += " " + options.awardDate;
-        blockquote.appendChild(dateP);
-    }
+		dateP.innerHTML += " " + options.awardDate;
+		blockquote.appendChild(dateP);
+	}
 
-    if (options.includeRecipientName && options.recipientName) {
-        const recipientP = document.createElement("p");
-        recipientP.className = "badgr-badge-recipient";
-        const recipientStrong = document.createElement("strong");
-        recipientStrong.setAttribute("style", "font-size: 12px; font-weight: bold; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #000;");
-        recipientStrong.innerHTML = "Awarded To:";
-        recipientP.appendChild(recipientStrong);
-        recipientP.setAttribute("style", "margin: 0; font-size: 12px; font-style: normal; font-stretch: normal; line-height: 1.67; letter-spacing: normal; text-align: left; color: #555555;");
+	if (options.includeRecipientName && options.recipientName) {
+		const recipientP = document.createElement("p");
+		recipientP.className = "badgr-badge-recipient";
+		const recipientStrong = document.createElement("strong");
+		recipientStrong.setAttribute(
+			"style",
+			optionNameStyle
+		);
+		recipientStrong.innerHTML = "Awarded To:";
+		recipientP.appendChild(recipientStrong);
+		recipientP.setAttribute(
+			"style",
+			optionValueStyle
+		);
 
-        const recipientSpan = document.createElement("span");
-        recipientSpan.style.display = "block";
-        recipientSpan.innerHTML = " " + options.recipientName;
-        recipientP.appendChild(recipientSpan);
-        blockquote.appendChild(recipientP);
-    }
+		const recipientSpan = document.createElement("span");
+		recipientSpan.style.display = "block";
+		recipientSpan.innerHTML = " " + options.recipientName;
+		recipientP.appendChild(recipientSpan);
+		blockquote.appendChild(recipientP);
+	}
 
-    if (options.includeVerifyButton) {
-        const verifyP = document.createElement("p");
-        verifyP.setAttribute("style", "margin: 16px 0; padding: 0;");
+	if (options.includeVerifyButton) {
+		const verifyP = document.createElement("p");
+		verifyP.setAttribute("style", "margin: 16px 0; padding: 0;");
 
-        const verifyTag = document.createElement("a");
-        verifyTag.className = "badgr-badge-verify";
-        verifyTag.setAttribute("target", "_blank");
-        verifyTag.setAttribute("href", "https://badgecheck.io?url="+options.shareUrl);
-        verifyTag.setAttribute("style", "margin: 0; font-size:14px; font-weight: bold; width: 48px; height: 16px; border-radius: 4px; border: solid 1px black; text-decoration: none; padding: 6px 16px; margin: 16px 0; color: black; ");
-        if (options.verified) {
-            var svgUrl = (options.staticPrefix || window.location.origin)+checkmarkSvg;
-            var checkImg = document.createElement("img");
-            checkImg.src = svgUrl;
-            verifyTag.appendChild(checkImg);
-            verifyTag.innerHTML += " VERIFIED!";
-            verifyTag.style.width = "90px";
-        } else {
-            verifyTag.innerHTML = "VERIFY";
-        }
+		const verifyTag = document.createElement("a");
+		verifyTag.className = "badgr-badge-verify";
+		verifyTag.setAttribute("target", "_blank");
+		verifyTag.setAttribute(
+			"href",
+			"https://badgecheck.io?url=" + options.shareUrl
+		);
+		verifyTag.setAttribute(
+			"style",
+			verifyButtonStyle
+		);
+		if (options.verified) {
+			var svgUrl =
+				(options.staticPrefix || window.location.origin) + checkmarkSvg;
+			var checkImg = document.createElement("img");
+			checkImg.src = svgUrl;
+			verifyTag.appendChild(checkImg);
+			verifyTag.innerHTML += " VERIFIED!";
+			verifyTag.style.width = "90px";
+		} else {
+			verifyTag.innerHTML = "VERIFY";
+		}
 
-        verifyP.appendChild(verifyTag);
-        blockquote.appendChild(verifyP);
-    }
+		verifyP.appendChild(verifyTag);
+		blockquote.appendChild(verifyP);
+	}
 
-    if (options.includeScript) {
-        var scriptUrl = (options.staticPrefix || window.location.origin)+"/widgets.bundle.js";
-        const widgetTag = document.createElement("script");
-        widgetTag.setAttribute("async","async");
-        widgetTag.setAttribute("src", scriptUrl);
-        blockquote.appendChild(widgetTag);
-    }
+	if (options.includeScript) {
+		var scriptUrl =
+			(options.staticPrefix || window.location.origin) + "/widgets.bundle.js";
+		const widgetTag = document.createElement("script");
+		widgetTag.setAttribute("async", "async");
+		widgetTag.setAttribute("src", scriptUrl);
+		blockquote.appendChild(widgetTag);
+	}
 
-    return blockquote;
+	return blockquote;
 }
-
