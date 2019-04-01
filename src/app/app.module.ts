69916f81ba9259fc74648f8e38e8358f196c4bb0
Ethan Sletteland <esletteland@concentricsky.com>
2019-04-01 15:50:59 -0700

    BGR-3584: Make analytics key pull from config.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -108,6 +108,18 @@ export const appInitializerFn = (configService: AppConfigService) => {
 		window["badgrConfig"] = config;
 
 		initializeTheme(configService);
+
+		// Google Tag Manager
+		const aKey = config.googleAnalytics.trackingId; // 'GTM-5DLJTBB';
+		const aScript = document.createElement('script');
+		aScript.text = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','${aKey}');`;
+
+		document.getElementsByTagName('head')[0].appendChild(aScript);
+
 	};
 };
 
