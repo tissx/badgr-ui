46df180ba042d70f22151fbb7f27cba97251d150
Ethan Sletteland <esletteland@concentricsky.com>
2019-05-07 14:14:08 -0700

    BGR-3358: Fix images.

--- src/app/auth/components/welcome/welcome.component.ts
+++ src/app/auth/components/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { preloadImageURL } from "../../../common/util/file-util";
 
 @Component({
   selector: 'app-welcome',
@@ -12,4 +13,12 @@ export class WelcomeComponent implements OnInit {
   ngOnInit() {
   }
 
+	readonly imageBadge = preloadImageURL(require("../../../../../node_modules/@concentricsky/badgr-style/dist/images/graphic-badge.svg") as string);
+	readonly imageBackpack = preloadImageURL(require("../../../../../node_modules/@concentricsky/badgr-style/dist/images/graphic-backpack.svg") as string);
+	readonly imageCollections = preloadImageURL(require("../../../../../node_modules/@concentricsky/badgr-style/dist/images/graphic-collections.svg") as string);
+	readonly imageIssuer = preloadImageURL(require("../../../../../node_modules/@concentricsky/badgr-style/dist/images/graphic-issuer.svg") as string);
+
 }
+
+
+
