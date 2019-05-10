c1b3114deac1bc349496a875d0c0414f3a38c767
Nathan Nichols <nnichols@concentricsky.com>
2019-05-10 12:25:35 -0700

    Group auth tests

--- cypress/integration/authentication/create-account.spec.js
+++ cypress/integration/authentication/create-account.spec.js
@@ -1,17 +1,20 @@
 /// <reference types="Cypress" />
 
 context('Window', () => {
-  beforeEach(() => {
+	beforeEach(() => {
 		cy.visit('http://localhost:4200/')
-		window.localStorage.setItem('config',`{"api":{"baseUrl":"http://localhost:7000"}}`)
-  })
+		window.localStorage.setItem('config', `{"api":{"baseUrl":"http://localhost:7000"}}`)
+	})
 
-  it('Create Account Errors and Login', () => {
-		// https://docs.cypress.io/api/commands/
-		cy.location('pathname').should('include','/auth/login')
+	// https://docs.cypress.io/api/commands/
+	it("take me to the login screen first", () => {
+		cy.location('pathname').should('include', '/auth/login')
 			.get('h1').should('exist')
 			.title().should('include', 'Badgr')
-			.get('nav.navbar > nav > div > a:nth-child(2)').click()
+	})
+
+	it("let me create and account and show errors", () => {
+		cy.get('nav.navbar > nav > div > a:nth-child(2)').click()
 			.get('#email').type('root@example.com')
 			.get('#first_name').type('Bob')
 			.get('#last_name').type('Flannigan')
@@ -26,11 +29,16 @@ context('Window', () => {
 			.get('bg-formfield-text.forminput.forminput-is-error > p').should('not.exist')
 			.get('.checkbox-x-errortext').should('not.exist')
 			.get('sign-up > div > form-message > div').should('exist')
-			.go('back')
+	})
+
+	it("allow me to sign in with credentials and open the mozilla modal", () => {
+		cy.get('nav.navbar > nav > div > a:nth-child(1)').click()
 			.get('#email').type('root@example.com')
-		  .get('#password').clear().type('12345678')
-		  .get('login > div > div > form > div > button').click()
-		  .get('.notification-x-text > h2').should('contain', 'Mozilla')
-		  .get('.notification-x-text > p > button').click()
-  })
+			.get('#password').clear().type('12345678')
+			.get('login > div > div > form > div > button').click()
+			.get('.notification-x-text > h2').should('contain', 'Mozilla')
+			.get('.notification-x-text > p > button').click()
+	})
+
+
 })
