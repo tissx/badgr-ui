f5451b28838363f3d0ea74a8ecc66aa08504db38
Nathan Nichols <nnichols@concentricsky.com>
2019-05-10 12:09:44 -0700

    Add cypress with basic password authentication test

---
+++ cypress/integration/authentication/create-account.spec.js
@@ -0,0 +1,36 @@
+/// <reference types="Cypress" />
+
+context('Window', () => {
+  beforeEach(() => {
+		cy.visit('http://localhost:4200/')
+		window.localStorage.setItem('config',`{"api":{"baseUrl":"http://localhost:7000"}}`)
+  })
+
+  it('Create Account Errors and Login', () => {
+		// https://docs.cypress.io/api/commands/
+		cy.location('pathname').should('include','/auth/login')
+			.get('h1').should('exist')
+			.title().should('include', 'Badgr')
+			.get('nav.navbar > nav > div > a:nth-child(2)').click()
+			.get('#email').type('root@example.com')
+			.get('#first_name').type('Bob')
+			.get('#last_name').type('Flannigan')
+			.get('#password_must_be_at_least_8_characters_').type('12345678')
+			.get('#confirm_password').type('12345679')
+			.get('form > div > div > button').click()
+			.get('bg-formfield-text.forminput.forminput-is-error > p').should('contain', 'not match')
+			.get('.checkbox-x-errortext').should('contain', 'Please read')
+			.get('#terms').click()
+			.get('#confirm_password').clear().type('12345678')
+			.get('form > div > div > button').click()
+			.get('bg-formfield-text.forminput.forminput-is-error > p').should('not.exist')
+			.get('.checkbox-x-errortext').should('not.exist')
+			.get('sign-up > div > form-message > div').should('exist')
+			.go('back')
+			.get('#email').type('root@example.com')
+		  .get('#password').clear().type('12345678')
+		  .get('login > div > div > form > div > button').click()
+		  .get('.notification-x-text > h2').should('contain', 'Mozilla')
+		  .get('.notification-x-text > p > button').click()
+  })
+})
