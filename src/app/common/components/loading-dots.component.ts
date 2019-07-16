56fe0a0b5fbbf18c9d66dd45e70b712742848521
Ethan Sletteland <esletteland@concentricsky.com>
2019-07-16 15:36:59 -0700

    BGR-4291: Update loader template.

--- src/app/common/components/loading-dots.component.ts
+++ src/app/common/components/loading-dots.component.ts
@@ -2,7 +2,11 @@ import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'loading-dots',
-    template: '<div class="loaderdots {{className}}"></div>'
+    template: `
+			<div class="l-flex l-flex-column l-flex-aligncenter l-flex-justifycenter u-margin-top2x {{className}}">
+				<div class="loaderspin loaderspin-large"></div>
+				<p class="u-text u-padding-top2x">Loading...</p>
+			</div>`
 })
 export class LoadingDotsComponent {
     @Input() className: string;
